898dd09a5ae8de63d3a95c7a44667660bc749b31
nishizoe <nishizoe@tejimaya.com>
2014-02-18 02:18:37 +0900

    migrationスクリプトの追加

---
+++ data/migrations/0.1.0/001_doctrine.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
+ * opKairanbanPluginMigrationVersion1
+ *
+ * @package    opKairanbanPlugin
+ * @author     Kaoru Nishizoe <nishizoe@tejimaya.com>
+ */
+class opKairanbanPluginMigrationVersion1 extends opMigration
+{
+  /**
+   * migration from version to 0.1.0
+   *
+   * *Not implemented yet. For reserving migration version 1.*
+   */
+  public function migrate($direction)
+  {
+  }
+}
